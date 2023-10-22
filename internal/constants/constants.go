8c29d0c93db6e71063b7a3ec6277dc8098a58c40
Tyler Gillson <tyler.gillson@gmail.com>
2023-10-22 17:39:59 -0600

    chore: update plugin code
    Signed-off-by: Tyler Gillson <tyler.gillson@gmail.com>

--- internal/constants/constants.go
+++ internal/constants/constants.go
@@ -1,7 +1,7 @@
 package constants
 
 const (
-	PluginCode                     string = "VSPHERE"
+	PluginCode                     string = "vSphere"
 	ValidationTypeRolePrivileges   string = "vsphere-role-privileges"
 	ValidationTypeEntityPrivileges string = "vsphere-entity-privileges"
 	ValidationTypeTag              string = "vsphere-tags"
