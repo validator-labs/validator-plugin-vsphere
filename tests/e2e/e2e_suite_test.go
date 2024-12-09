7018e501af22178f5a146d7fd8b64f1d9153508e
Artur Shad Nik <arturshadnik@gmail.com>
2024-12-09 15:11:25 -0800

    test: get e2e tests working
    Signed-off-by: Artur Shad Nik <arturshadnik@gmail.com>

--- tests/e2e/e2e_suite_test.go
+++ tests/e2e/e2e_suite_test.go
@@ -42,7 +42,7 @@ var (
 
 	// projectImage is the name of the image which will be build and loaded
 	// with the code source changes to be tested.
-	projectImage = "example.com/validator-plugin-vsphere:v0.0.1"
+	projectImage = "quay.io/validator-labs/validator-plugin-vsphere:latest"
 )
 
 // TestE2E runs the end-to-end (e2e) test suite for the project. These tests execute in an isolated,
