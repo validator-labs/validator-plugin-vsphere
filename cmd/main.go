a61215a6f49da9fc67be626c6ad69a2aa7249063
Artur Shad Nik <arturshadnik@gmail.com>
2024-12-09 20:40:06 -0800

    fix: move nosec to right spot, add log back in
    Signed-off-by: Artur Shad Nik <arturshadnik@gmail.com>

--- cmd/main.go
+++ cmd/main.go
@@ -144,6 +144,7 @@ func main() {
 
 	if err = (&controller.VsphereValidatorReconciler{
 		Client: mgr.GetClient(),
+		Log:    ctrl.Log.WithName("controllers").WithName("NetworkValidator"),
 		Scheme: mgr.GetScheme(),
 	}).SetupWithManager(mgr); err != nil {
 		setupLog.Error(err, "unable to create controller", "controller", "VsphereValidator")
